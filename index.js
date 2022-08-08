e37763893e25fa17c7406bc9db212b649d4feff7
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-09 02:21:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(catName) {
+  cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat(lastCat) {
+  cats.pop(lastCat);
+}
+
+function destructivelyRemoveFirstCat(firstCat) {
+  cats.shift(firstCat);
+}
+
+
+function appendCat(newCatEnd) {
+  const newCat = [...cats];
+  newCat.push(newCatEnd);
+  return newCat;
+}
+
+function prependCat(newCatInitial) {
+  const newCatPrepended = [...cats];
+  newCatPrepended.unshift(newCatInitial);
+  return newCatPrepended;
+}
+
+
+
+function removeLastCat() {
+  const newCatsWithoutLast = cats.slice(0, -1);
+  return newCatsWithoutLast;
+}
+
+function removeFirstCat() {
+  const newCatsWithoutFirst = cats.slice(1);
+  return newCatsWithoutFirst;
+}
